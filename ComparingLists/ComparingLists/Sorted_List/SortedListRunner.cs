a25a4fab792c12072859142ce087b8d2fb33ede6
ktlhape <ktlhape@iie.ac.za>
2023-08-17 08:24:47 +0200

    Sorted List

---
+++ ComparingLists/ComparingLists/Sorted_List/SortedListRunner.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparingLists.Sorted_List
+{
+    internal class SortedListRunner
+    {
+        static void Main(string[] args)
+        {
+            #region numbersList
+            SortedList<int, string> numbers = new
+                SortedList<int, string>();
+
+            numbers.Add(7, "Seven");
+            numbers.Add(1, "One");
+            numbers.Add(12, "Twelve");
+            numbers.Add(5, "Five");
+            numbers.Add(4, "Four");
+
+            //Display all the elements
+            //DisplayItems(numbers);
+            numbers.Remove(1);
+            numbers.RemoveAt(2);
+            //remove element at index 2 (7)
+            //DisplayItems(numbers);
+            //4,5,12
+
+            #endregion
+
+            /*Create a sorted list to store departments 
+             * with a list of employees per department.
+             
+             you can use IT, HR, FINANCE, etc */
+            SortedList<string, List<string>> departments =
+                new SortedList<string, List<string>>();
+
+            departments.Add("IT", new List<string>()
+            { "Kabelo","Roger","Michael","Tiger"});
+
+            departments.Add("HR", new List<string>()
+            { "Thabo","Jessica","Carol"});
+
+            departments.Add("FINANCE", new List<string>()
+            { "James","Lesego","David","Serena","Lerato"});
+
+            //Display department data
+
+            foreach (KeyValuePair<string,List<string>> dep in departments)
+            {
+                Console.Write(dep.Key);
+                foreach (string employees in dep.Value)
+                {
+                    Console.WriteLine($"\t{employees}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void DisplayItems(SortedList<int, string> numbers)
+        {
+            foreach (KeyValuePair<int, string> num in numbers)
+            {
+                Console.WriteLine(num.Key + " - " + num.Value);
+            }
+            Console.WriteLine("===========================");
+        }
+    }
+}
--- SortingAlgorithm/SortingAlgorithm/Program.cs
+++ SortingAlgorithm/SortingAlgorithm/Program.cs
@@ -11,21 +11,81 @@ namespace SortingAlgorithm
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[99999];
-            Random rdn = new Random();
-            for (int i = 0; i < numbers.Length; i++)
+            // int[] numbers = new int[99999];
+            //create a list to store student objects
+            List<Student> students = new List<Student>()
             {
-                numbers[i] = rdn.Next(1,1500);
-            }
+                new Student("Kelvin","Smith",21),
+                new Student("Roger","Federer",18),
+                new Student("Michael","Jordan",18),
+                new Student("Serena","Williams",23),
+                new Student("Tiger","Woods",20)
+            };
 
+            /*Create a method that will sort the 
+             * students by age*/
+            Console.WriteLine("=====Before Sort=====\n");
+            DisplayStudents(students);
+            SortStudents(students);
+            Console.WriteLine("\n=====After sort======");
+            DisplayStudents(students);
+
+            List<int> numbers = new List<int>();
+            Random rdn = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                numbers.Add(rdn.Next(1,1500));
+            }
             //Console.WriteLine("=====Before Sort=====");
             //DisplayElements(numbers);
-
-            BubbleSort(numbers);
-            SelectionSort(numbers);
-            Console.WriteLine("\n=====After Sort=====");
+            //BubbleSort(numbers);
+            //SelectionSort(numbers);
+            //InsertionSort(numbers);
+            //Console.WriteLine("\n=====After Sort=====");
             //DisplayElements(numbers);
             Console.Read();
+        }
+        private static void DisplayStudents(List<Student> students)
+        {
+            foreach (Student st in students)
+            {
+                Console.WriteLine(st.ToString());
+            }
+        }
+        private static void SortStudents(List<Student> stList)
+        {
+            for (int i = 0; i < stList.Count - 1; i++)
+            {
+                for (int j = (i + 1); j < stList.Count; j++)
+                {
+                    if (stList[i].Age > stList[j].Age)
+                    {
+                        Student temp = stList[i];
+                        stList[i] = stList[j];
+                        stList[j] = temp;
+                    }
+                }
+            }
+        }
+
+        private static void InsertionSort(int[] ar)
+        {
+            Stopwatch stw = new Stopwatch();
+            stw.Start();
+            for (int i = 0; i < ar.Length  - 1; i++)
+            {
+                for (int j = (i + 1); j > 0; j--)
+                {
+                    if (ar[j - 1] < ar[j])
+                    {
+                        int temp = ar[j - 1];
+                        ar[j - 1] = ar[j];
+                        ar[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine($"Insertion Sort: {stw.ElapsedMilliseconds}");
         }
         private static void SelectionSort(int[] ar)
         {
@@ -49,15 +109,13 @@ namespace SortingAlgorithm
             }
             Console.WriteLine($"Selection Sort: {stw.ElapsedMilliseconds}");
         }
-
-        private static void DisplayElements(int[] numbers)
+        private static void DisplayElements(List<int> numbers)
         {
             foreach (int num in numbers)
             {
                 Console.Write(num + " ");
             }
         }
-
         private static void BubbleSort(int[] ar)
         {
             Stopwatch stw = new Stopwatch();
@@ -71,6 +129,25 @@ namespace SortingAlgorithm
                         int temp = ar[j];
                         ar[j] = ar[x];
                         ar[x] = temp;
+                    }
+                }
+            }
+            Console.WriteLine($"Bubble Sort: {stw.ElapsedMilliseconds}");
+        }
+
+        private static void BubbleSort(List<int> ls)
+        {
+            Stopwatch stw = new Stopwatch();
+            stw.Start();
+            for (int x = 0; x < ls.Count - 1; x++)
+            {
+                for (int j = (x + 1); j < ls.Count; j++)
+                {
+                    if (ls[j] < ls[x])
+                    {
+                        int temp = ls[j];
+                        ls[j] = ls[x];
+                        ls[x] = temp;
                     }
                 }
             }
