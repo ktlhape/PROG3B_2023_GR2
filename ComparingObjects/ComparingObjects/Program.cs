cf24ab0b5b95303f45a65b6b921f9702439ea1f0
ktlhape <ktlhape@iie.ac.za>
2023-09-11 12:38:25 +0200

    Comparing objects by implementing the IComparable interface

---
+++ ComparingObjects/ComparingObjects/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace ComparingObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new() { 
+            new("David","Jones",20000),
+            new("Roger","Federer",20000),
+            new("Novak","Djokovic",25000),
+            new("Samantha","Jones",21000),
+            new("Mika","Federer",19000),
+            new("Kevin","De Bruyne",26000),
+            new("Mike","Ross",19500),
+            new("Jessica","Jones",20000)
+            };
+
+            Console.WriteLine("=======Before Sort======");
+            Display(employees);
+            BubbleSort(employees);
+            Console.WriteLine("\n=======After Sort======");
+            Display(employees);
+
+        }
+
+        private static void BubbleSort(List<Employee> strList)
+        {
+            for (int i = 0; i < strList.Count - 1; i++)
+            {
+                for (int j = (i + 1); j < strList.Count; j++)
+                {
+                    if (strList[i].CompareTo(strList[j]) > 0)
+                    {
+                        Employee temp = strList[i];
+                        strList[i] = strList[j];
+                        strList[j] = temp;
+                    }
+                }
+            }
+        }
+        
+        private static void Display(List<Employee> employees)
+        {
+            foreach (Employee em in employees)
+            {
+                Console.WriteLine(em.ToString());
+            }
+        }
+    }
+    public class Employee : IComparable<Employee>
+    {
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public double Salary { get; set; }
+        public Employee(string firstname, string lastname, double sal) =>
+            (Firstname, Lastname, Salary) = (firstname, lastname, sal);
+
+        public override string? ToString()
+        {   
+            return $"{Firstname} {Lastname.ToUpper()} : {Salary.ToString("C2")}";
+        }
+           
+
+        public int CompareTo(Employee? other)
+        {
+            int results = this.Salary.CompareTo(other.Salary);
+            if (results == 0)
+            {
+                results = this.Firstname.CompareTo(other.Firstname);
+            }
+            return results;
+        }
+    }
+}
---
+++ OperatorOverloading/OperatorOverloading/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+
+namespace OperatorOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box boxA = new Box(4, 6);
+            Box boxB = new Box(3, 8);
+            //Box boxC = new Box(boxA.Length + boxB.Length, boxA.Width + boxB.Width);
+            Box boxC = boxA + boxB;
+
+            boxA++;
+            bool results = boxA > boxB;
+
+            /*overload the operator such that the
+             * length and width of a box increases 
+             * by a percentage*/
+
+            Console.WriteLine(boxC.ToString());
+
+
+
+
+
+
+        }
+    }
+
+    public class Box
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Box(double theLength, double theWidth) => 
+            (Length, Width) = (theLength, theWidth);
+
+        public override string? ToString()
+        {
+            return $"Length: {Length}\nWidth: {Width}\n" +
+                $"==================";
+        }
+
+        //modifier static return type operator + (operands)
+        public static Box operator +(Box b1, Box b2) =>
+         new Box(b1.Length + b2.Length, b1.Width + b2.Width);
+
+        public static Box operator ++(Box b)
+        {
+            return new Box(b.Length + 1, b.Width + 1);
+        }
+        //public static int operator +(Box b1, Box b2)
+        //{
+        //    return 0;
+        //}
+        public static bool operator >(Box b1, Box b2) =>
+            b1.Length > b2.Length && b1.Width > b2.Width;
+        public static bool operator <(Box b1, Box b2) =>
+            b1.Length < b2.Length && b1.Width < b2.Width;
+
+    }
+}
