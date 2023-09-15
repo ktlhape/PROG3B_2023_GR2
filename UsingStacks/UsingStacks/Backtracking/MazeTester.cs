46799cad3041b1c63e31e0214ebe0eebf8d4a0c2
ktlhape <ktlhape@iie.ac.za>
2023-09-15 08:13:12 +0200

    using stacks

---
+++ UsingStacks/UsingStacks/Backtracking/MazeTester.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingStacks.Backtracking
+{
+    internal class MazeTester
+    {
+       static int[,] maze = new int[,]{
+        { 1,1,0,0},
+        { 1,0,0,0},
+        { 1,0,1,0},
+        { 0,0,1,1}};
+        /*create a 2D array "path" that has the same
+         * number of rows and columns as the maze*/
+        static int[,] path = new int[4, 4];
+        static void Main(string[] args)
+        {
+            FindPath(0, 0, 4);
+            for (int row = 0; row < maze.GetLength(0); row++)
+            {
+                for (int col = 0; col < maze.GetLength(1); col++)
+                {
+                    if (path[row,col] == 1)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                    }
+                }
+            }
+            Console.Read();
+        }
+        static int FindPath(int row, int col, int size)
+        {
+            if (row == size - 1 && col == size - 1) return 1;
+            if (maze[row,col] == 1)
+            {
+                path[row, col] = 1;
+                if (FindPath(row, col + 1, size) == 1) return 1;
+                if (FindPath(row + 1, col, size) == 1) return 1;
+
+                path[row, col] = 0;
+            }
+            return 0;
+      
+        }
+    }
+}
---
+++ UsingStacks/UsingStacks/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace UsingStacks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = "today is tuesday";
+            string reverseWords = "";
+            Stack<char> stk = new();
+
+            for (int i =  0; i < words.Length; i++)
+            {
+                stk.Push(words[i]); //y
+                
+            }
+
+            foreach (char c in stk)
+            {
+                reverseWords += c;
+            }
+
+            Console.WriteLine(reverseWords);
+
+        }
+    }
+}
